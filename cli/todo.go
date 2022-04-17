69b48b7bb16f5a968c551697e17cdac7a526d933
jeremaihloo <jeremaihloo1024@gmail.com>
2022-04-17 08:23:23 +0800

    update

--- cli/todo.go
+++ cli/todo.go
@@ -1,10 +1,20 @@
 package cli
 
+import (
+	"context"
+
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/mr-doggy/doggy-sdk-go/doggy"
+)
+
 type TodoOp struct {
+	api *doggy.APIClient
 }
 
-func NewTodoOp() *TodoOp {
-	return &TodoOp{}
+func NewTodoOp(api *doggy.APIClient) *TodoOp {
+	return &TodoOp{
+		api: api,
+	}
 }
 
 func (op *TodoOp) Create() error {
@@ -19,6 +29,28 @@ func (op *TodoOp) Delete() error {
 	return nil
 }
 
-func (op *TodoOp) List() error {
-	return nil
+func (op *TodoOp) List(pageIndex, pageSize int32) error {
+	special, _, err := op.api.ItemApi.ApiAppItemSpecialItemsGet(context.Background()).Execute()
+	if err != nil {
+		return nil
+	}
+	var noteParent doggy.ItemDto
+	for _, item := range special.Items {
+		if item.Type == doggy.NOTE.Ptr() {
+			noteParent = item
+			break
+		}
+	}
+	result, _, err := op.api.ItemApi.ApiAppItemGet(context.Background()).
+		ParentItemId(*noteParent.Id).
+		SkipCount((pageIndex - 1) * pageSize).
+		MaxResultCount(pageSize).
+		Execute()
+	if err != nil {
+		return err
+	}
+	prompt := &survey.Select{
+		Message: "Choose a color:",
+		Options: []string{"red", "blue", "green"},
+	}
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,5 @@ module github.com/mr-doggy/doggy-sdk-go
 go 1.13
 
 require (
-	github.com/AlecAivazis/survey/v2 v2.3.4
-	github.com/spf13/cobra v1.4.0
-	github.com/spf13/viper v1.11.0
-	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5
+	golang.org/x/oauth2 v0.0.0-20210218202405-ba52d332ba99
 )
